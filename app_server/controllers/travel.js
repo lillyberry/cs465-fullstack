510a9bc766037bce458c3ccf8983424eb58988be
Lilly Berry <lmberry123@gmail.com>
2025-07-26 16:29:03 -0400

    Module 3 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,11 @@
-const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+var fs = require('fs');
+var path = require('path');
+
+var tripsPath = path.join(__dirname, '../../data/trips.json');
+var trips = JSON.parse(fs.readFileSync(tripsPath, 'utf8'));
+
+var travel = function(req, res) {
+    res.render('travel', { title: 'Travlr Getaways', trips: trips });
 };
 
-module.exports = { 
-    travel
-};
+module.exports = { travel };
