7ffba3bb0d51899eff818816b2a6079178a2e6fb
Lilly Berry <lmberry123@gmail.com>
2025-08-03 18:41:05 -0400

    Module 5 completed baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,32 @@
-var fs = require('fs');
-var path = require('path');
-
-var tripsPath = path.join(__dirname, '../../data/trips.json');
-var trips = JSON.parse(fs.readFileSync(tripsPath, 'utf8'));
-
-var travel = function(req, res) {
-    res.render('travel', { title: 'Travlr Getaways', trips: trips });
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
 };
 
-module.exports = { travel };
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('./data/trips.json','utf8'));
+
+const travel = async function ( req, res, next) {
+    await fetch(tripsEndpoint,options)
+        .then((res) => res.json())
+        .then((json) => {
+            let message =null;
+            if (!(json instanceof Array)){
+                message = "API lookup error";
+                json = [];
+            } else {
+                if (!json.length) {
+                    message = "No trips exist in our database!";
+                }
+            }
+            res.render("travel", {title: "Travlr Getaways", trips: json, message});
+        })
+        .catch((err) => res.status(500).send(err.message));
+};
+
+module.exports = {
+    travel,
+};
